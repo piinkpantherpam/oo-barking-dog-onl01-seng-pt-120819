4f309a88417f2ba2100663e752b6c5f1c7f02593
Pam Cairo <pcairo_90@outlook.com>
2020-01-08 23:00:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,21 +1,4 @@
 class Dog
-  def name=(dog_name)
-    @this_dogs_name = dog_name
-  end
-  
-def name
-  @this_dogs_name
-  end
-end
-
-class Dog 
-  def bark 
-    puts = "woof!"
-  end
-  
-end
-
-class Dog
   def name=(dogs_name)
     @name = dogs_name
   end 
